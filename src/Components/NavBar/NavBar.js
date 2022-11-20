cc9b84622dbf8a45e3346564588cac5c36c1dffb
imtiazho <axubair9@gmail.com>
2022-11-20 21:26:02 +0600

    Environment setup and complete navbar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+      <div className="nav-container">
+        <h3>Phone Deal</h3>
+
+        <div className="nav-items">
+          <Link to="home">Home</Link>
+          <Link to="blog">Blog</Link>
+          <Link to="about">About</Link>
+
+          {
+            <>
+              <Link to="myItems">My Items</Link>
+              <Link to="addItems">Add Items</Link>
+              {0 ? (
+                <Link to="login">Sign Out</Link>
+              ) : (
+                <Link to="login">Login</Link>
+              )}
+            </>
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
